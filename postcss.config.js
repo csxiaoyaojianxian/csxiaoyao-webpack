949f5903026223c1c5e93ff78a5a1d31672fe99c
csxiaoyao <1724338257@qq.com>
2017-11-12 22:09:08 +0800

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /*
-* @Author: victorsun
+* @Author: csxiaoyao
 * @Date:   2017-09-05 10:43:01
-* @Last Modified by:   victorsun
+* @Last Modified by:   csxiaoyao
 * @Last Modified time: 2017-09-06 14:54:57
 */
 
